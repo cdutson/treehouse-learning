be321a1d23f22b8ed267c7cf7200a08e7af853af
Corey Dutson <coreyd@gadventures.com>
2015-07-19 19:30:37 -0400

    Initial Commit

---
+++ simple iphone app with swift/FunFact/FunFact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFact
+//
+//  Created by Corey Dutson on 2015-07-19.
+//  Copyright (c) 2015 Corey Dutson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
