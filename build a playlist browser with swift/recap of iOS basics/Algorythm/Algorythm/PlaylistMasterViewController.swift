a6b56eb122e7d0f242a2acedf97da6b3c90ed36a
Corey Dutson <coreyd@gadventures.com>
2015-07-28 18:18:32 -0400

    made a sample app for learning transitions. First couple parts of the stage

---
+++ build a playlist browser with swift/recap of iOS basics/Algorythm/Algorythm/PlaylistMasterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Algorythm
+//
+//  Created by Corey Dutson on 2015-07-28.
+//  Copyright (c) 2015 Corey Dutson. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistMasterViewController: UIViewController {
+
+    @IBOutlet weak var aButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        aButton.setTitle("Press Me", forState: .Normal)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showPlaylistDetail" {
+            let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
+            playlistDetailController.segueLabelText = "Yay! You pressed the button!"
+        }
+    }
+}
+
